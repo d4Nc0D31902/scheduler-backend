2c5cc70cebc49ec07f51c68187295ec51857562e
d4Nc0D31902 <danielrodri1902@gmail.com>
2024-02-19 12:05:17 +0800

    app-env

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+
 const products = require("./routes/product");
 const appointments = require("./routes/appointment");
 const locations = require("./routes/location");
@@ -14,20 +15,17 @@ const borrows = require("./routes/borrow");
 const auth = require("./routes/auth");
 const order = require("./routes/order");
 const errorMiddleware = require("./middlewares/errors");
+
 app.use(express.json({ limit: "100mb" }));
-// app.set("trust proxy", 1);
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
+app.use(cookieParser());
+
 app.use(
-  // cors({
-  //   origin: "http://localhost:3000",
-  //   credentials: true,
-  // })
   cors({
     origin: "https://scheduler-frontend-mu.vercel.app",
     credentials: true,
   })
 );
-app.use(cookieParser());
-app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
 app.use("/api/v1", products);
 app.use("/api/v1", auth);
@@ -42,4 +40,5 @@ app.use("/api/v1", categories);
 app.use("/api/v1", borrows);
 
 app.use(errorMiddleware);
+
 module.exports = app;
