5c8909a1709bffdf0fbeab5fe3bb8854e1cf49f2
d4Nc0D31902 <danielrodri1902@gmail.com>
2024-02-19 10:09:29 +0800

    appjs

--- app.js
+++ app.js
@@ -17,8 +17,12 @@ const errorMiddleware = require("./middlewares/errors");
 app.use(express.json({ limit: "100mb" }));
 // app.set("trust proxy", 1);
 app.use(
+  // cors({
+  //   origin: "http://localhost:3000",
+  //   credentials: true,
+  // })
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://scheduler-frontend-mu.vercel.app",
     credentials: true,
   })
 );
