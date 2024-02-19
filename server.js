d2bc63709bb7a339ff6d627f1dee1e5a987d6524
d4Nc0D31902 <danielrodri1902@gmail.com>
2024-02-19 10:05:59 +0800

    app-server

--- server.js
+++ server.js
@@ -1,27 +1,19 @@
-const express = require("express");
-const app = express();
-const connectDatabase = require("./config/database");
-const path = require("path");
-const cloudinary = require("cloudinary");
+const app = require('./app');
+const connectDatabase = require('./config/database')
+const path = require('path')
+const cloudinary = require('cloudinary')
 
-require("dotenv").config({ path: "./config/.env" });
+require('dotenv').config({path:  './config/.env'});
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 connectDatabase();
-console.log(process.env.DATABASE);
-if (process.env.NODE_ENV !== "PRODUCTION")
-  require("dotenv").config({ path: "backend/config/.env" });
-
-app.get("/", (req, res) => {
-  res.send("Server is Online and Ready to Go");
-});
-
+console.log(process.env.DATABASE)
+if (process.env.NODE_ENV !== 'PRODUCTION') 
+	require('dotenv').config({ path: 'backend/config/.env' })
 app.listen(process.env.PORT, () => {
-  console.log(
-    `Server started on port: ${process.env.PORT} in ${process.env.NODE_ENV} mode`
-  );
+	console.log(`server started on port:' ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
 });
