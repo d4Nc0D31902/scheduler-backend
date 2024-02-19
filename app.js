410900562fa90fb78f1f0aeec796c08b3fd5a622
d4Nc0D31902 <danielrodri1902@gmail.com>
2024-02-19 12:12:07 +0800

    app

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-
 const products = require("./routes/product");
 const appointments = require("./routes/appointment");
 const locations = require("./routes/location");
@@ -15,20 +14,21 @@ const borrows = require("./routes/borrow");
 const auth = require("./routes/auth");
 const order = require("./routes/order");
 const errorMiddleware = require("./middlewares/errors");
-
-// Enable CORS at the top of the middleware chain
+app.use(express.json({ limit: "100mb" }));
+// app.set("trust proxy", 1);
 app.use(
+  // cors({
+  //   origin: "http://localhost:3000",
+  //   credentials: true,
+  // })
   cors({
     origin: "https://scheduler-frontend-mu.vercel.app",
     credentials: true,
   })
 );
-
-app.use(express.json({ limit: "100mb" }));
-app.use(express.urlencoded({ limit: "100mb", extended: true }));
 app.use(cookieParser());
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
-// Define routes
 app.use("/api/v1", products);
 app.use("/api/v1", auth);
 app.use("/api/v1", order);
@@ -41,7 +41,5 @@ app.use("/api/v1", sports);
 app.use("/api/v1", categories);
 app.use("/api/v1", borrows);
 
-// Error middleware should be at the end
 app.use(errorMiddleware);
-
 module.exports = app;
