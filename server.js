6835ef35f82e8b646f21fbe5d5df6cc41eab4a79
d4Nc0D31902 <danielrodri1902@gmail.com>
2024-02-19 09:30:15 +0800

    update vercel

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
-const app = require('./app');
-const connectDatabase = require('./config/database')
-const path = require('path')
-const cloudinary = require('cloudinary')
+const express = require("express");
+const app = express();
+const connectDatabase = require("./config/database");
+const path = require("path");
+const cloudinary = require("cloudinary");
 
-require('dotenv').config({path:  './config/.env'});
+require("dotenv").config({ path: "./config/.env" });
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 connectDatabase();
-console.log(process.env.DATABASE)
-if (process.env.NODE_ENV !== 'PRODUCTION') 
-	require('dotenv').config({ path: 'backend/config/.env' })
+console.log(process.env.DATABASE);
+if (process.env.NODE_ENV !== "PRODUCTION")
+  require("dotenv").config({ path: "backend/config/.env" });
+
+app.get("/", (req, res) => {
+  res.send("Server is Online and Ready to Go");
+});
+
 app.listen(process.env.PORT, () => {
-	console.log(`server started on port:' ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
+  console.log(
+    `Server started on port: ${process.env.PORT} in ${process.env.NODE_ENV} mode`
+  );
 });
