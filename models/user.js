1a7006871ff570ade8a632cb1bc882de17e0a542
d4Nc0D31902 <danielrodri1902@gmail.com>
2024-02-22 21:08:10 +0800

    user

--- models/user.js
+++ models/user.js
@@ -38,7 +38,7 @@ const userSchema = new mongoose.Schema(
     avatar: {
       public_id: {
         type: String,
-        required: true,
+        // required: true,
       },
       url: {
         type: String,
